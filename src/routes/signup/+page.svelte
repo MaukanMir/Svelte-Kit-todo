ad13a44e233e247474ab0b02d3b64662d346d6d1
Maukan Mir <maukanm22@gmail.com>
2023-01-22 16:59:33 -0500

    Added an edit button

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import Nav from "../../lib/Nav.svelte";
     import taskDataBase from "../../stores/taskDataBase";
+    import MdModeEdit from 'svelte-icons/md/MdModeEdit.svelte'
 
     let editGoal = "";
     let editHow ="";
@@ -9,42 +10,56 @@
 
     const editTask = () =>{
 
+        
+        taskDataBase.update(currentGoals =>{
+
+            return [...currentGoals]
+        })
+
     };
-    
+
 </script>
 
 <Nav/>
+<h1 class ="title">Edit Goals</h1>
     <div class ="parent">
     <form class ="form" on:submit|preventDefault={editTask}>
     {#each $taskDataBase as task}
         <div class = "inside-form">
             <label for="goal">Original Goal: {task.goal}</label>
-            <input
+            <!-- <input
+            class =""
             type="text"
-            />
+            /> -->
+            <button class ="edit-icon"> <MdModeEdit/> </button>
         </div>
 
         <div class = "inside-form">
             <label for="editHow"> Original How: {task.how}</label>
-            <input
+            <!-- <input
+            class =""
             type="text"
-            />
+            /> -->
+            <button class ="edit-icon"> <MdModeEdit/> </button>
         </div>
 
         <div class = "inside-form">
             <label for="editDate"> Original Date: {task.date}</label>
-            <input
+            <!-- <input
+            class =""
             type="date"
-            />
+            /> -->
+        <button class ="edit-icon"> <MdModeEdit/> </button>
         </div>
         {/each}
     </form>
 </div>
 
 
-
 <style>
-
+    .title{
+        text-align: center;
+    }
     .parent{
         display:flex;
         flex-direction: column;
@@ -59,6 +74,13 @@
 
 .inside-form label{
     font-size: 20px;
+    margin: 10px;
+}
+
+.inside-form input{
+    width: 100%;
+    border-radius: 6px;
+    font-size: 16px;
 }
 
 .inside-form{
@@ -67,5 +89,17 @@
     border: 2px solid black;
     margin:10px;
     padding:10px;
+}
+
+.edit-icon{
+    height: 50px;
+    margin: 10px;
+    cursor:pointer;
+    background-color: white;
+    border:none;
+}
+
+.edit-icon:hover{
+    color:red;
 }
 </style>
