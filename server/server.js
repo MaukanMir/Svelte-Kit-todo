d10a9ceb556e45ab41626239851f930ff09181c2
Maukan Mir <maukanm22@gmail.com>
2023-02-04 18:47:33 -0500

    Added warning removal for depracated feature

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ var cors = require('cors');
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const createGoalsRoute = require("./routes/createGoal")
 
 //Middleware
@@ -14,22 +13,16 @@ app.use(cors());
 app.use("/api/createGoals", createGoalsRoute);
 
 
-const client = new MongoClient(
-    process.env.MONGO_URL, 
-    { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true, 
-        serverApi: ServerApiVersion.v1 
-    });
+// get rid of annyoing error
+mongoose.set("strictQuery",false);
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
     console.log("connected to db")
+    app.listen(port,()=>{
+        console.log("Backend server is running on: ", port)
+    })
 })
 .catch((err)=>{
     console.log(err)
-})
-
-app.listen(port,()=>{
-    console.log("Backend server is running on: ", port)
 })
