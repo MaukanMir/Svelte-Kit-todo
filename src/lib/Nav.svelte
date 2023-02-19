c562e12e1e5e15f4d9de74b048fe7a7fda2551e7
Maukan Mir <maukanm22@gmail.com>
2023-02-19 10:41:37 -0500

    Fixed registration and user login appearing at the same time

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,10 +1,22 @@
+<script>
+    // Svelte store import here
+    import {userDataBase} from "../stores/userDataBase"
+
+    
+</script>
+
 <nav class ="test">
+    {#each $userDataBase as user}
     <a href="/">Home</a>
     <a href="/goals">Goals</a>
     <a href="/editgoals">Edit Goals</a>
     <a href="/creategoals">Create Goals</a>
+    {#if !user === false}
     <a href="/register">Register</a>
+    {:else}
     <a href="/login">Login</a>
+    {/if}
+    {/each}
 </nav>
 
 <style>
