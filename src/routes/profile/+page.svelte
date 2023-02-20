bba466208b7a7f9afdce727cab79240c235c31e9
Maukan Mir <maukanm22@gmail.com>
2023-02-19 19:28:45 -0500

    Added new styling to commit page

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -8,6 +8,14 @@
 
     let user = get(userDataBase)[0];
 
+    //edit profile
+
+    let editUsername = "";
+    let editPassword ="";
+    let editEmail ="";
+
+
+
 </script>
 
 <!--HTML Components Begin Here-->
@@ -15,12 +23,65 @@
 
     <Nav/>
 
-    
+    <div class ="container">
+
+        {#if user}
+
+        <form class="inside-container">
+
+            <div class ="form">
+                <label for ="editUsername">Edit Username</label>
+                <input
+                type="text"
+                required
+                placeholder="Username"
+                bind:value={editUsername}
+                />
+            </div>
+
+            <div class ="form">
+                <label for ="editEmail">Edit Email</label>
+                <input
+                type="text"
+                required
+                placeholder="Email"
+                bind:value={editEmail}
+                />
+            </div>
+
+            <div class ="form">
+                <label for ="editPassword">Edit Password</label>
+                <input
+                type="password"
+                required
+                placeholder="Password"
+                bind:value={editPassword}
+                />
+            </div>
+
+        </form>
+
+        {/if}
+
+    </div>
+
 
     <Footer/>
 </body>
 
 
 <style>
+    body{
+        height:100vh;
+        background-color: #000036;
+    }
+
+    .container{
+        display: flex;
+        justify-content: center;
+    }
+
+
+
 
 </style>
