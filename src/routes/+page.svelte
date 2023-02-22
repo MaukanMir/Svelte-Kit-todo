0547cfed002d9186a914acff15bafee52ba00657
Maukan Mir <maukanm22@gmail.com>
2023-02-22 08:55:24 -0500

    Added card styling to home page using flexbox, looks better but its not terrible

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,12 @@
     // import components here
     import Nav from "../lib/Nav.svelte";
     import Footer from "../lib/Footer.svelte"
-    // svelete components imported here
+    // svelte components imported here
     import {onMount} from "svelte";
     // Svelte store import here
     import {userDataBase} from "../stores/userDataBase";
     import {get} from "svelte/store"
+    // Svelte routing imports here
 
     const user = get(userDataBase)[0]
     console.log(user)
@@ -33,8 +34,10 @@
 
 <h1 class ="header">Goal Analysis</h1>
 
+
 <div class ="parent">
     {#each posts as task}
+
         <div class ="inside-parent"> 
             <h3>Goal: {task.goal}</h3>
             <h3>Start Date:{task.date}</h3>
@@ -44,8 +47,10 @@
             <h3>Days Remaining: {Math.round(Math.abs(new Date(task.date).getTime() - currentTime) / oneDay)}</h3>
             <h3>Percentage Completed: {  Math.round(Math.abs(new Date(task.date).getTime() - currentTime)/ oneDay ) / Math.round(Math.abs(new Date(task.setDate).getTime() - currentTime) / oneDay)}%</h3>
         </div>
+
     {/each}
 </div>
+
 
 <Footer/>
 
@@ -61,36 +66,21 @@
     .header{
         text-align: center;
     }
-
     .parent{
         display: flex;
-        align-items: center;
+        flex-wrap: wrap;
         justify-content: space-between;
     }
 
     .inside-parent{
-        display: flex;
-        flex-wrap:wrap;
-        border: 6px solid white;
-        margin:10px;
         background-color: white;
         border-radius:10px;
         color:#000036;
-        flex:2;
+        flex: 0 1 calc(25% - 1em);
+        margin:10px;
+        padding:20px;
     }
 
-    .inside-parent h2{
-        margin: 20px 10px;
-        padding: 10px;
-        display: flex;
-        border-bottom: 6px solid #000036;
-    }
-
-    .inside-parent h2:hover{
-        color:white;
-        cursor: pointer;
-        background-color: red;
-    }
 
 </style>
 
