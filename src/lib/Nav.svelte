f91875e85c49e00b351ed6d7f0ff938b594b89f8
Maukan Mir <maukanm22@gmail.com>
2023-03-04 12:07:21 -0500

    Added new user styles

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -9,10 +9,10 @@
 <nav>
 
     <div class="nav">
-        <button> <a href="/">Home</a></button>
+        <button> <a href="/" class ="home">Home</a></button>
 
         <div class="goals">
-            <button> <a href="/goals">Goals</a></button>
+            <button>Goals</button>
             <ul>
                 <li><a href="/goals">Create Goals</a></li>
                 <li><a href="/editgoals">Edit Goals</a></li>
@@ -20,7 +20,6 @@
                 <li><a href="/login">Login</a></li>
                 <li><a href="/register">Register</a></li>
             </ul>
-
         </div>
     </div>
 
@@ -50,7 +49,73 @@
 
     body{
         font-family: sans-serif;
+        margin:0px;
+        padding:0px;
     }
+
+    .nav{
+        display:flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        margin:auto;
+    }
+
+    .home, .goals button{
+        background-color:#1e1e1e;
+        border:none;
+        cursor: pointer;
+        font-size: 2em;
+        color:#fff;
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    button{
+        background-color: #1e1e1e;
+        border:none;
+    }
+
+
+
+    .goals ul{
+        list-style: none;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+        position:absolute;
+        left: -90px;
+        width: 200px;
+        height:200px;
+    }
+
+    .goals{
+        position: relative;
+    }
+
+    .goals ul a{
+        text-decoration: none;
+        color:#fff;
+        opacity: 1;
+        font-size: 1.5em;
+    }
+
+    .goals button:focus + ul{
+        opacity: 1;
+        pointer-events: all;
+        transform: translateY(0px);
+        z-index: -1;
+    }
+
+    /* .goals a:hover{
+        color:blueviolet;
+        cursor: pointer;
+    } */
+
+
+
+
 
 
 </style>
