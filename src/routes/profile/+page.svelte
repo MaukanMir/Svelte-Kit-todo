04805621268901d8e3ed704130f0dedd92bb5f94
Maukan Mir <maukanm22@gmail.com>
2023-03-07 07:49:59 -0500

    Fixed some spelling issues with the user login page

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -59,7 +59,7 @@
     <Nav/>
 
     <div class ="welcome">
-        <h1 class ="welcome-header">Welcome Back, {user}!</h1>
+        <h1 class ="welcome-header">Welcome Back, {user[0].toUpperCase() + user.substring(1)}!</h1>
     </div>
     <div class ="center"> 
     <div class ="profile">
