00ebf212d3f14c6358b7d5237802784932f8c0a1
Maukan Mir <maukanm22@gmail.com>
2023-04-08 16:32:02 -0400

    Made changes

--- src/stores/userSession.js
+++ src/stores/userSession.js
@@ -1,19 +1,22 @@
+// @ts-nocheck
 import { writable } from 'svelte/store';
 
 function createSessionStorageStore(key, initialValue) {
-  // if(localStorage != undefined){
-  //   const storedValue = localStorage.getItem(key);
-  //   const initial = storedValue !== null ? JSON.parse(storedValue) : initialValue;
-  //   const store = writable(initial);
   
-  //   store.subscribe(value => {
-  //     localStorage.setItem(key, JSON.stringify(value));
-  //   });
+  if(typeof window === 'undefined'){
+    writable(initialValue)
+
+  }else{
+    const storedValue = localStorage.getItem(key);
+    const initial = storedValue !== null ? JSON.parse(storedValue) : initialValue;
+    const store = writable(initial);
   
-  //   return store;
-  // }else{
-  //   console.log("no localStorage")
-  // }
+    store.subscribe(value => {
+      localStorage.setItem(key, JSON.stringify(value));
+    });
+  
+    return store;
+  }
 
 }
 
