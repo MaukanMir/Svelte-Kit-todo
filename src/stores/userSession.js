b1509ade38bf67142332c8b3041040497a10d17d
Maukan Mir <maukanm22@gmail.com>
2023-04-08 16:39:25 -0400

    Added new files

--- src/stores/userSession.js
+++ src/stores/userSession.js
@@ -2,22 +2,26 @@
 import { writable } from 'svelte/store';
 
 function createSessionStorageStore(key, initialValue) {
+  let value = initialValue;
   
-  if(typeof window === 'undefined'){
-    writable(initialValue)
+  if (typeof window !== 'undefined' && window.localStorage) {
+    const storedValue = window.localStorage.getItem(key);
+    if (storedValue !== null) {
+      value = JSON.parse(storedValue);
+    }
 
-  }else{
-    const storedValue = localStorage.getItem(key);
-    const initial = storedValue !== null ? JSON.parse(storedValue) : initialValue;
-    const store = writable(initial);
+    const store = writable(value);
   
     store.subscribe(value => {
-      localStorage.setItem(key, JSON.stringify(value));
+      window.localStorage.setItem(key, JSON.stringify(value));
     });
-  
+
     return store;
   }
 
+  const store = writable(value);
+
+  return store;
 }
 
 export const userSession = createSessionStorageStore('userSession', {
