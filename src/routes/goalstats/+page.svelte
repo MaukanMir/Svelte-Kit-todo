b6aea4c46fb5cdd470adb247d558a0a281e1b67d
Maukan Mir <maukanm22@gmail.com>
2023-04-17 15:21:12 -0400

    Added new styles

--- src/routes/goalstats/+page.svelte
+++ src/routes/goalstats/+page.svelte
@@ -159,6 +159,7 @@ import Nav from "../../lib/Nav.svelte";
                     streak: check === true ? interval[0].streak+1:0,
                 })
             })
+            
             console.log(res.status)
 
         }
